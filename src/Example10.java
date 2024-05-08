5c5d93ea5e896bd3361a9b881dd63001766590df
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-08 17:25:12 +0300

    Update Example10.java

--- src/Example10.java
+++ src/Example10.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Example10 {
 //    Packages
+    /*
+    A package in Java is used to group related classes.
+     */
+
+    public static void main(String[] args){
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+
+         String name = scanner.nextLine();
+         
+        System.out.println("Your name is: " + name);
+        System.out.println("Hello and welcome to my App!");
+    }
 }
