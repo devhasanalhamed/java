86983582e1a4903dc3d3ebb9a23584c48da9972d
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-08 17:06:35 +0300

    Update Example9.java

--- src/Example9.java
+++ src/Example9.java
@@ -1,3 +1,29 @@
 public class Example9 {
+//    Encapsulation
+    /*
+    The meaning of Encapsulation,
+    is to make sure that "sensitive" data is hidden from users.
 
+    1- declare class variables/attributes as private
+    2- provide public get and set methods to access and update the value of a private variable
+     */
+
+    public static void main(String[] args){
+        final Encapsulation enc = new Encapsulation();
+        enc.setName("Hasan");
+        System.out.println(enc.getName());
+    }
+
+}
+
+class Encapsulation {
+    private String name;
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getName(){
+        return name;
+    }
 }
