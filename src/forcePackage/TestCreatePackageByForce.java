93eb26f6643870d7199e8c87f22719f469a7366b
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-09 09:26:16 +0300

    Create TestCreatePackageByForce.java

---
+++ src/forcePackage/TestCreatePackageByForce.java
@@ -0,0 +1,7 @@
+package forcePackage;
+public class TestCreatePackageByForce {
+    /// This package created using the context actions fix
+    public static void main(String[] args) {
+        System.out.println("This forced package is created!");
+    }
+}
