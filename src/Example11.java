dfbd9302fe1cc47c935560642ae47fa9a0faf90c
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-09 10:18:01 +0300

    Update Example11.java

--- src/Example11.java
+++ src/Example11.java
@@ -1,2 +1,29 @@
+// Java inheritance
+
 public class Example11 {
+    public static void main(String[] args){
+        final Car car = new Car();
+
+        car.honk();
+
+        System.out.println("Beautiful "+ car.modelName+ " is it " + car.model + "?");
+
+    }
+}
+
+
+// final class Vehicle { // This will cause an error because final class can't be inherited
+  class Vehicle {
+    protected String brand = "Ford";
+
+    public void honk(){
+        System.out.println("Tuut, tuut!");
+    }
+}
+
+class Car extends Vehicle {
+    final String modelName = "F-150 Raptor";
+    final int model = 2025;
+
+
 }
