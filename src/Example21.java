a02e1f313a7909bccb4b04d694556550ac8805c3
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-11 18:58:57 +0300

    Create Example21.java

---
+++ src/Example21.java
@@ -0,0 +1,13 @@
+public class Example21 {
+    //    Wrapper
+    /*
+    Classes provide a way to use primitive
+     data types (int, boolean, etc..) as objects.
+     */
+
+    public static void main(String[] args) {
+        Integer x = 4;
+
+        System.out.println(x);
+    }
+}
