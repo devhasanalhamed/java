a37cf633d8dee7fcbeda37ce808d9b728f8748b6
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-17 23:24:21 +0300

    Update Example27.java

--- src/Example27.java
+++ src/Example27.java
@@ -1,2 +1,33 @@
+import java.util.HashMap;
+
 public class Example27 {
+
+    public static int repetitions(String s) {
+        final String[] arr = s.split("");
+        int repeat = 1;
+        String chr = " ";
+        int localCounter = 1;
+        for (String letter : arr) {
+            if (letter.equals(chr)) {
+                localCounter += 1;
+            } else {
+                chr = letter;
+                localCounter = 1;
+            }
+
+            if (localCounter > repeat) {
+
+                repeat = localCounter;
+            }
+        }
+        return repeat;
+    }
+
+
+    public static void main(String[] args) {
+        final int result = repetitions("AAAAAAAAAA");
+        System.out.println(result);
+
+
+    }
 }
