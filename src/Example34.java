f053686212289cb899d1848df45be51b4f201cc0
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-20 23:42:01 +0300

    Update Example34.java

--- src/Example34.java
+++ src/Example34.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Example34 {
+import java.util.Arrays;
+
+public class Example34 {
+    public static void main(String[] args) {
+        int[] x = {1, 5, 87, 6, 3};
+        Arrays.sort(x);
+        System.out.println(x);
+
+    }
 }
