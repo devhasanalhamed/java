efdf0808b2cfdcf2cc6a998b88a14922c96dab9b
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-24 23:24:39 +0300

    Update Example34.java

--- src/Example34.java
+++ src/Example34.java
@@ -1,10 +1,37 @@
 import java.util.Arrays;
 
 public class Example34 {
+
+
     public static void main(String[] args) {
-        int[] x = {1, 5, 87, 6, 3};
-        Arrays.sort(x);
-        System.out.println(x);
+        class Person {
+            int age;
+
+            Person(int initialAge) {
+                if (initialAge < 0) {
+                    age = 0;
+                    System.out.println("Age is not valid, setting age to 0.");
+                }
+                age = initialAge;
+
+            }
+
+            void yearPasses() {
+                age += 1;
+            }
+
+            void amIOld() {
+                if (age < 13) {
+                    System.out.println("You are young.");
+                } else if (age < 18) {
+                    System.out.println("You are teenager.");
+                } else {
+                    System.out.println("You are old.");
+                }
+            }
+
+        }
+
 
     }
 }
