d386635179c54f8da75d9aefe07ca4770f73b006
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-05-24 23:41:47 +0300

    Update Example34.java

--- src/Example34.java
+++ src/Example34.java
@@ -1,37 +1,52 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Example34 {
 
+    public static class Person {
+        int age;
 
-    public static void main(String[] args) {
-        class Person {
-            int age;
-
-            Person(int initialAge) {
-                if (initialAge < 0) {
-                    age = 0;
-                    System.out.println("Age is not valid, setting age to 0.");
-                }
+        Person(int initialAge) {
+            if (initialAge < 0) {
+                age = 0;
+                System.out.println("Age is not valid, setting age to 0.");
+            } else {
                 age = initialAge;
-
-            }
-
-            void yearPasses() {
-                age += 1;
-            }
-
-            void amIOld() {
-                if (age < 13) {
-                    System.out.println("You are young.");
-                } else if (age < 18) {
-                    System.out.println("You are teenager.");
-                } else {
-                    System.out.println("You are old.");
-                }
             }
 
         }
 
+        public void amIOld() {
+            if (age < 13) {
+                System.out.println("You are young.");
+            } else if (age >= 13 && age < 18) {
+                System.out.println("You are a teenager.");
+            } else {
+                System.out.println("You are old.");
+            }
+        }
 
+        public void yearPasses() {
+            age += 1;
+        }
     }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int age = scanner.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        scanner.close();
+    }
+
 }
