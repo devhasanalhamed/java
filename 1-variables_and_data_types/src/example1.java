4bb6e259009a82ce64a5e2168342dc49032c0521
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-06-02 17:02:13 +0300

    Update example1.java

--- 1-variables_and_data_types/src/example1.java
+++ 1-variables_and_data_types/src/example1.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.Scanner;
 
 public class example1 {
@@ -8,12 +6,21 @@ public class example1 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("================================");
-        for(int i = 0; i < 3; i++){
-            String txt = scanner.next();
-            int num = scanner.nextInt();
-            System.out.printf("%-15s%03d%n", txt, num);
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++){
+            int a = scanner.nextInt();
+            System.out.println(a);
+            int b = scanner.nextInt();
+            System.out.println(b);
+            int n = scanner.nextInt();
+            System.out.println(n);
+            int accumulator = 0;
+            for (int j = 0; j < n; j++){
+                int result = (int) (accumulator + (Math.pow(2,j) * b));
+                accumulator = result;
+                System.out.print(result + a + " ");
+            }
+            System.out.println();
         }
-        System.out.println("================================");
     }
 }
