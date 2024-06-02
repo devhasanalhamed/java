b6a9c70f352a9fce8a76bbb46085d067f5a33492
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-06-02 06:58:10 +0300

    Update example2.java

--- src/example2.java
+++ src/example2.java
@@ -1,29 +1,38 @@
 public class example2 {
 
     public static void main(String[] args) {
-    // we need to declare variable: datatype and name
-    // initialize variable with a value
+
+        String x = "dev hasan alhamed";
+        System.out.println("I am " + x);
+        System.out.println("I am American citizen");
+        System.out.print("I am married \n");
+        System.out.println("I am a dream catcher");
+        // java is a case-sensitive
+        // every statement ends with semicolon;
+
+
+        // we need to declare variable: datatype and name
+        // initialize variable with a value
 
         int score;
-        score  = 6;
+        score = 6;
         System.out.println(score);
         score = 10;
         System.out.println(score);
         score += score;
         System.out.println(score);
-        score ++;
+        score++;
         System.out.println(score);
 
         int apples = 3;
         int oranges = 2;
 
-        System.out.println("apples: " + apples + " oranges: "+ oranges);
+        System.out.println("apples: " + apples + " oranges: " + oranges);
         System.out.println("fruits: " + apples + oranges);
 
-        System.out.println(5*8);
-        System.out.println(5+8);
+        System.out.println(5 * 8);
+        System.out.println(5 + 8);
     }
-
 
 
 }
