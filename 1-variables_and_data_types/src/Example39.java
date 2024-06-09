18b6bbf4a3a24127a968316c643dc7e32083dc16
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-06-09 21:05:50 +0300

    Update Example39.java

--- 1-variables_and_data_types/src/Example39.java
+++ 1-variables_and_data_types/src/Example39.java
@@ -3,8 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Example39 {
     //    Hackerrank day 8
