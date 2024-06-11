5bce2321ff4c4f222f7d9dff58d9c05251483315
Hassan Al-Hamed <therealhassan97@gmail.com>
2024-06-11 14:19:34 +0300

    Update example2.java
    replace lesson two

--- 1-variables_and_data_types/src/example2.java
+++ 1-variables_and_data_types/src/example2.java
@@ -2,37 +2,43 @@ public class example2 {
 
     public static void main(String[] args) {
 
-        String x = "dev hasan alhamed";
-        System.out.println("I am " + x);
-        System.out.println("I am American citizen");
-        System.out.print("I am married \n");
-        System.out.println("I am a dream catcher");
-        // java is a case-sensitive
-        // every statement ends with semicolon;
+//        Java has 8 primitive datatypes
+//        char, boolean,
+//        byte, short, int, long,
+//        float, double.
+//
+//        Integers: are a whole number, positive, negative, or zero
+//        Signed integer: means that one of the bits in the byte
+//        indicate whether the number is positive or negative
+//        (MSB -> 0: positive, 1: negative).
+//
+//        With 8 bits, an unsigned byte (without a sign bit) can represent
+//        values from 0 to 255 (2 raised to the power of 8 minus 1).
+//
+//        Because one bit is used for the sign, a signed byte
+//        can represent values from -128 to 127.
+//
+//        Primitives that hold integer:
+//        byte: 8 bit signed integer.
+//        short: 16 bit signed integer.
+//        int: 32 bit signed integer.
+//        long: 64 bit signed integer.
+//        https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+
+        //Assigning different type of integers
+        byte b = 4;
+        short sh = 16;
+        int i = 32;
+        // Long prefix: L
+        long l = 6_421_321_321L;
+
+        System.out.println("byte: " + b);
+        System.out.println("int: " + i);
+        System.out.println("short: " + sh);
+        System.out.println("long: " + l);
+        // Add separators to a number
+        System.out.printf("long with separator: %,d", l);
 
 
-        // we need to declare variable: datatype and name
-        // initialize variable with a value
-
-        int score;
-        score = 6;
-        System.out.println(score);
-        score = 10;
-        System.out.println(score);
-        score += score;
-        System.out.println(score);
-        score++;
-        System.out.println(score);
-
-        int apples = 3;
-        int oranges = 2;
-
-        System.out.println("apples: " + apples + " oranges: " + oranges);
-        System.out.println("fruits: " + apples + oranges);
-
-        System.out.println(5 * 8);
-        System.out.println(5 + 8);
     }
-
-
 }
